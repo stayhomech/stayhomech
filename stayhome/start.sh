9d2a4bc6ebcbb6f9c74dc0bb53fb2861fca25dd8
Alexandre Georges <alexandre@georges.gs>
2020-03-20 13:11:11 +0100

    Added mo files to gitignore as they are now generated on startup. Improved startup script to reduce restart time.

--- stayhome/start.sh
+++ stayhome/start.sh
@@ -6,24 +6,32 @@ python manage.py migrate --noinput
 # Create admin user
 python manage.py createsuperuser --no-input
 
-# Import data
+# Import data, only with new repo
+# Do it manually in container later if necessary
+# This is to avoid a very long startup delay
 if [ -f "datasets/post_data.csv.bz2" ]; then
+
+    # Uncompress Swiss Post dataset
     cd datasets
     bunzip2 post_data.csv.bz2
     cd ..
-fi
-if [ -f "datasets/post_data.csv" ]; then
+
+    # Import Swiss Post dataset - Creates municipalities and postal codes (NPA)
     python manage.py geo_import datasets/post_data.csv --debug
-fi
-if [ -f "datasets/cantons.csv" ]; then
+
+    # Import list of Swiss cantons
     python manage.py geo_import_cantons datasets/cantons.csv
-fi
-if [ -f "datasets/districts.csv" ]; then
+
+    # Import list of Swiss districts
     python manage.py geo_import_districts datasets/districts.csv
-fi
-if [ -f "datasets/municipalities.csv" ]; then
+
+    # Attach municipalities to districts
     python manage.py geo_import_municipalities datasets/municipalities.csv
+
 fi
+
+# Compile language files
+python manage.py compilemessages
 
 # Statics
 python manage.py collectstatic --noinput
