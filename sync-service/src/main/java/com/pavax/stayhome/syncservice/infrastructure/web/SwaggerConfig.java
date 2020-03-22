b0b540821b491fe7cea73e1d0912a83c98d903d6
Patrick Dobler <patrick.dobler@gmail.com>
2020-03-22 22:23:23 +0100

    #34: added actuator config

--- sync-service/src/main/java/com/pavax/stayhome/syncservice/infrastructure/web/SwaggerConfig.java
+++ sync-service/src/main/java/com/pavax/stayhome/syncservice/infrastructure/web/SwaggerConfig.java
@@ -4,17 +4,22 @@ import static springfox.documentation.builders.PathSelectors.regex;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Map;
 
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableSwagger2
 class SwaggerConfig {
+
+	@Value("${management.endpoints.web.base-path}")
+	String managementContextPath;
 
 	@Bean
 	public Docket publicApi() {
@@ -24,8 +29,18 @@ class SwaggerConfig {
 				.build()
 				.directModelSubstitute(LocalDate.class, java.sql.Date.class)
 				.directModelSubstitute(LocalDateTime.class, java.util.Date.class)
-			/*	.securityContexts(Collections.singletonList(securityContext()))
-				.securitySchemes(Collections.singletonList(apiKey()))*/
 				;
 	}
+
+	@Bean
+	public Docket managementApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("manage-api")
+				.select()
+				.paths(regex(managementContextPath + ".*"))
+				.build()
+				.ignoredParameterTypes(Map.class)
+				;
+	}
+
 }
