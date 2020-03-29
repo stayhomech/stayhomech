8981e17acf22a5d388bb8b4664fb8aca7545a95b
Patrick Dobler <patrick.dobler@gmail.com>
2020-03-29 18:32:48 +0200

    Fix: let spring boot build an H2 in memory db

--- integrations/localhero/src/main/java/ch/stayhome/integrations/localhero/LocalHeroApp.java
+++ integrations/localhero/src/main/java/ch/stayhome/integrations/localhero/LocalHeroApp.java
@@ -4,12 +4,13 @@ import static org.springframework.boot.SpringApplication.run;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EnableBatchProcessing
 public class LocalHeroApp {
+
 	public static void main(String[] args) {
 		run(LocalHeroApp.class, args);
 	}
+
 }
