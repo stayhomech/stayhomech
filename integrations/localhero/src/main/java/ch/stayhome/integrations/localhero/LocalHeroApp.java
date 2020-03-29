e3eebebfa71ef1b168cda42d43f1a0e4ec16eb7a
Patrick Dobler <patrick.dobler@gmail.com>
2020-03-29 19:01:27 +0200

    Fix: cleanup

--- integrations/localhero/src/main/java/ch/stayhome/integrations/localhero/LocalHeroApp.java
+++ integrations/localhero/src/main/java/ch/stayhome/integrations/localhero/LocalHeroApp.java
@@ -4,9 +4,11 @@ import static org.springframework.boot.SpringApplication.run;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableBatchProcessing
+@EnableScheduling
 public class LocalHeroApp {
 
 	public static void main(String[] args) {
