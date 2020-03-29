30f03cd8de2f94384f6c31f9650016900c7f7344
Patrick Dobler <patrick.dobler@gmail.com>
2020-03-29 22:51:47 +0200

    Fix: revert h2-db dependency

--- integrations/localhero/src/main/java/ch/stayhome/integrations/localhero/LocalHeroApp.java
+++ integrations/localhero/src/main/java/ch/stayhome/integrations/localhero/LocalHeroApp.java
@@ -4,9 +4,10 @@ import static org.springframework.boot.SpringApplication.run;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableBatchProcessing
 @EnableScheduling
 public class LocalHeroApp {
