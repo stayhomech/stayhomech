2b5ac90562887441f0c6081a86cae1a3c6f46d12
Beat Luginbühl <lugi@lugipfupf.ch>
2020-04-08 00:11:03 +0200

    #3 more reliably extract URLs from any text

--- integrations/common/src/main/java/ch/stayhome/integrations/utils/ContactInformationGuesser.java
+++ integrations/common/src/main/java/ch/stayhome/integrations/utils/ContactInformationGuesser.java
@@ -1,15 +1,19 @@
 package ch.stayhome.integrations.utils;
 
-import static org.apache.commons.lang3.StringUtils.trim;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import com.google.i18n.phonenumbers.PhoneNumberMatch;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.Phonenumber;
+import com.linkedin.urls.Url;
+import com.linkedin.urls.detection.UrlDetector;
+import com.linkedin.urls.detection.UrlDetectorOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.apache.commons.lang3.StringUtils.trim;
 
 
 public class ContactInformationGuesser {
@@ -17,7 +21,7 @@ public class ContactInformationGuesser {
     private final PhoneNumberUtil util = PhoneNumberUtil.getInstance();
 
     // copied from: https://stackoverflow.com/a/17773849
-    private static final Pattern WEBSITE_PATTER = Pattern.compile("(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})");
+    private static final Pattern WEBSITE_PATTERN = Pattern.compile("(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})");
 
     private static final Pattern EMAIL_PATTERN = Pattern.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+");
 
@@ -42,14 +46,30 @@ public class ContactInformationGuesser {
         return results.isEmpty() ? "" : trim(results.get(0));
     }
 
-    public String extractWebsite(String text) {
-        text = cleanupString(text);
-        Matcher matcher = WEBSITE_PATTER.matcher(text);
-        final List<String> results = new ArrayList<>();
-        while (matcher.find()) {
-            results.add(matcher.group());
+    public String extractWebsite(final String text) {
+        final String cleanedupText = cleanupString(text);
+        final List<Url> foundUrls = new UrlDetector(cleanedupText, UrlDetectorOptions.HTML).detect();
+
+
+        if (foundUrls.isEmpty()) {
+            return "";
+        } else {
+            Optional<Url> optionalUrl = foundUrls.stream()
+                    .filter(url -> {
+                        // prefer https
+                        if (url.getScheme().equalsIgnoreCase("https")) {
+                            return true;
+                        }
+
+                        // if no https, double check, whether an email address was raped, since
+                        // UrlDetector creates this abominaton: foo@example.com --> http://foo@example.com
+                        return cleanedupText.contains(url.getFullUrl());
+                    })
+                    .findFirst()
+                    .or(Optional::empty);
+
+            return optionalUrl.isPresent() ? optionalUrl.get().getFullUrl() : "";
         }
-        return results.isEmpty() ? "" : trim(results.get(0));
     }
 
     public String extractStreet(String text) {
--- integrations/common/src/test/java/ch/stayhome/integrations/utils/ContactInformationGuesserTest.java
+++ integrations/common/src/test/java/ch/stayhome/integrations/utils/ContactInformationGuesserTest.java
@@ -1,9 +1,9 @@
 package ch.stayhome.integrations.utils;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 class ContactInformationGuesserTest {
@@ -13,6 +13,30 @@ class ContactInformationGuesserTest {
     @BeforeEach
     void setUp() {
         guesser = new ContactInformationGuesser();
+    }
+
+    @Test
+    public void testExtractComplexInfo() {
+        // given
+        final String text = "\n" +
+                "<p>Aufgrund der aktuellen Lage wegen der Covid19-Pandemie bleibt der Schalter der Neutralen Beratung " +
+                "Treuhand GmbH bis auf Weiteres geschlossen. <strong>Die Neutrale Beratung Treuhand ist per E-Mail und " +
+                "per Telefon <a href=\"http://+41326237191\" data-wplink-url-error=\"true\">032 623 71 91</a> " +
+                "erreichbar</strong>.</p><p>Sie können Ihre Unterlagen neben unserem Büro in den grossen Briefkasten " +
+                "legen oder uns diese per Post zusenden. Für Rückfragen bitte einfach eine Telefonnummer dazu schreiben. " +
+                "Ihre Steuererklärung wird dann wie gewohnt von uns für Sie erstellt.</p><p>Um eine Abgabefrist per " +
+                "31. Juli garantieren zu können, müssten die Unterlagen bis spätestens am 30.06.2020 bei uns eingegangen " +
+                "sein.</p><p>Wir bedanken uns für Ihr Verständnis und wünschen Ihnen und Ihrer Familie alles Gute.</p>" +
+                "<p>Neutrale Beratung Treuhand GmbH<br />Rötistrasse 3, 4500 Solothurn</p><p>Thomas Hodel &amp; Heidi " +
+                "Pfister<br /><a href=\"tel:+41326237191\">032 623 71 91</a></p>" +
+                "<p><a href=\"https://www.steuerprofis.ch/\">www.steuerprofis.ch</a></p>";
+
+
+        // when
+        final String result = this.guesser.extractWebsite(text);
+
+        // then
+        assertThat(result).isEqualTo("https://www.steuerprofis.ch/");
     }
 
     @Test
@@ -41,6 +65,30 @@ class ContactInformationGuesserTest {
 
         // then
         assertThat(result).isEqualTo("info@chuchilade.ch");
+    }
+
+    @Test
+    public void extractNoWebsite() {
+        // given
+        final String text = "This does not contain a website";
+
+        // when
+        final String result = this.guesser.extractWebsite(text);
+
+        // then
+        assertThat(result).isEqualTo("");
+    }
+
+    @Test
+    public void extractNoWebsiteButEmail() {
+        // given
+        final String text = "Here is just an email address: foo@example.com and it should not be extracted";
+
+        // when
+        final String result = this.guesser.extractWebsite(text);
+
+        // then
+        assertThat(result).isEqualTo("");
     }
 
     @Test
