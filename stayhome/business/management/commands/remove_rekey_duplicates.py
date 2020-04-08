7caa8797193c3a0e12dd8a779346b49ce7b6797a
Alexandre Georges <alexandre@georges.gs>
2020-04-08 22:49:03 +0200

    Needed changes to better match keys

--- stayhome/business/management/commands/remove_rekey_duplicates.py
+++ stayhome/business/management/commands/remove_rekey_duplicates.py
@@ -39,6 +39,7 @@ class Command(BaseCommand):
         new_c = 0
         old_found = 0
         old_not_found = 0
+        old_multi_found = 0
 
         old_count = {}
 
@@ -72,8 +73,34 @@ class Command(BaseCommand):
 
                 except Request.DoesNotExist:
 
-                    # Old not found, we can consider that this is a new request
-                    old_not_found += 1
+                    try:
+                        old_request = Request.objects.filter(name=request.name, location=request.location, creation__range=['1999-01-01', '2020-04-07'], source=2, source_uuid__startswith='DerBund-')
+
+                        if old_request.count() == 1:
+
+                            old_request = old_request[0]
+
+                            # Old found, but we have to check if multiple match
+                            old_found += 1
+                        
+                            if old_request.source_uuid in old_count:
+                                old_count[old_request.source_uuid].append(new_key)
+                            else:
+                                old_count[old_request.source_uuid] = [new_key]
+
+                        elif old_request.count() > 1:
+
+                            old_multi_found += 1
+
+                        else:
+
+                            # Old not found, we can consider that this is a new request
+                            old_not_found += 1
+
+                    except Request.DoesNotExist:
+
+                        # Old not found, we can consider that this is a new request
+                        old_not_found += 1
 
             else:
                 print('Found an unexpected key groups count: %d' % len(key))
@@ -81,6 +108,7 @@ class Command(BaseCommand):
 
         print('Found %d old keys and %d new keys!' % (old_c, new_c))
         print('%d new requests have a matching old request. %d do not.' % (old_found, old_not_found))
+        print('%d new requests had multiple match using looser algo' % old_multi_found)
 
         unique_c = 0
         non_unique_c = 0
@@ -125,4 +153,3 @@ class Command(BaseCommand):
         print(non_uniques)
 
         exit()
-
