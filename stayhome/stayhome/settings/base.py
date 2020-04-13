774fd9042f0213a9ea18e69e17736d506836b007
Alexandre Georges <alexandre@georges.gs>
2020-04-13 17:34:28 +0200

    Authentication issue on pre-prod

--- stayhome/stayhome/settings/base.py
+++ stayhome/stayhome/settings/base.py
@@ -145,7 +145,7 @@ WHITENOISE_ROOT = os.path.join(BASE_DIR, 'rootfiles')
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        #'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'stayhome.utils.rest.StayHomeAccessPermission',
